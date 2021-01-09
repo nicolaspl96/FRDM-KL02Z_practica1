a8ad8b8cc5429b0324df74eb442eb3fb366a2caf
ErnestoARC <ernesto.arc@gmail.com>
2021-01-08 22:31:35 -0500

    Función que gestiona IRQ de UART0 usando buffer circular

--- FRDMKL02Z_Project_UART/sdk_hal/sdk_hal_uart0.c
+++ FRDMKL02Z_Project_UART/sdk_hal/sdk_hal_uart0.c
@@ -10,11 +10,15 @@
  * Includes
  ******************************************************************************/
 #include "sdk_hal_uart0.h"
-
+#include "fsl_lpsci.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+/*! @brief Define nombre interno de funcion para gestionar interrupción por dato UART */
+#define UART0_IRQ_HANDLER 		UART0_IRQHandler
 
+/*! @brief Tamaño de buffer circular para recibir datos por UART (Unit: Byte). */
+#define DEMO_RING_BUFFER_SIZE 	64
 /*******************************************************************************
  * Private Prototypes
  ******************************************************************************/
@@ -28,12 +32,36 @@
 /*******************************************************************************
  * Local vars
  ******************************************************************************/
+uint8_t demoRingBuffer[DEMO_RING_BUFFER_SIZE];
+volatile uint16_t txIndex; /* Index of the data to send out. */
+volatile uint16_t rxIndex; /* Index of the memory to save new arrived data. */
 
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
+/*--------------------------------------------*/
+/*!
+ * @brief Funcion que gestiona IRQ por UART0
+ */
+void UART0_IRQ_HANDLER(void)
+{
+    uint8_t data;
 
+    if ((kLPSCI_RxDataRegFullFlag)&LPSCI_GetStatusFlags(UART0))
+    {
+        data = LPSCI_ReadByte(UART0);
 
+        /* If ring buffer is not full, add data to ring buffer. */
+        if (((rxIndex + 1) % DEMO_RING_BUFFER_SIZE) != txIndex)
+        {
+            demoRingBuffer[rxIndex] = data;
+            rxIndex++;
+            rxIndex %= DEMO_RING_BUFFER_SIZE;
+        }
+    }
+}
+/*--------------------------------------------*/
 /*******************************************************************************
  * Public Source Code
  ******************************************************************************/
+/*--------------------------------------------*/
