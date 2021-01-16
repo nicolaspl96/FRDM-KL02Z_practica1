690f10dd9e0d7146d37ee1e34d7c8458581fd992
LAPTOP-H9RDQ0N4\max_i <polonicolas96@gmail.com>
2021-01-16 00:11:41 -0500

    arreglo de cada uno de los led, con su respectiva activacion, dependiendo mayuscula o minuscula

--- FRDMKL02Z_Project_UART/source/FRDMKL02Z_Project_UART.c
+++ FRDMKL02Z_Project_UART/source/FRDMKL02Z_Project_UART.c
@@ -1,37 +1,3 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
-/**
- * @file    FRDMKL02Z_Project_UART.c
- * @brief   Application entry point.
- */
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -51,6 +17,28 @@
  */
 int main(void) {
   	/* Init board hardware. */
+
+	SIM->SCGC5|=1<<10;// activa la señal del reloj
+	  	int t=0;
+	  	int s=10000000;//velocidad de colores
+
+//           para el pin 6, led rojo
+	  	PORTB->PCR[6]|=1<<8;//activa puertob pin como gpio
+	  	  	GPIOB->PDDR|=1<<6;//configura pin 10 como salida
+	  		GPIOB->PCOR|=0<<6;//cambia el valor en el pin
+	  		GPIOB->PTOR|=1<<6;
+//           para el pin 10 led azul
+	  		PORTB->PCR[10]|=1<<8;//activa puertob pin como gpio
+	  		  	GPIOB->PDDR|=1<<10;//configura pin 10 como salida
+	  			GPIOB->PCOR|=0<<10;//cambia el valor en el pin
+	  			GPIOB->PTOR|=1<<10;
+//         para el pin 7 led verde
+	  			PORTB->PCR[7]|=1<<8;//activa puertob pin como gpio
+	  			  	GPIOB->PDDR|=1<<7;//configura pin 10 como salida
+	  				GPIOB->PCOR|=0<<7;//cambia el valor en el pin
+	  				GPIOB->PTOR|=1<<7;
+
+
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
@@ -60,7 +48,9 @@ int main(void) {
 #endif
 
     (void)uart0Inicializar(115200);
-
+unsigned int contador_ledblue=0;
+unsigned int contador_ledred=0;
+unsigned int contador_ledgreen=0;
     while(1) {
     	status_t status;
     	uint8_t nuevo_byte;
@@ -69,6 +59,31 @@ int main(void) {
     		status=uart0LeerByteDesdeBufferCircular(&nuevo_byte);
     		if(status==kStatus_Success){
     			printf("dato:%c\r\n",nuevo_byte);
+    			if(nuevo_byte == 'R' && contador_ledred==0){
+    			    				GPIOB->PTOR =1<<6;
+    			    				contador_ledred=contador_ledred+1;
+    			    			}
+    			if(nuevo_byte == 'r' && contador_ledred!=0){
+    			    			     GPIOB->PTOR =1<<6;
+    			    			     contador_ledred=0;
+    			} // led rojo
+    			if(nuevo_byte == 'G' && contador_ledgreen==0){
+    			    GPIOB->PTOR =1<<7;
+    			    contador_ledgreen=contador_ledgreen+1;
+    			    			    }
+    			    if(nuevo_byte == 'g' && contador_ledgreen!=0){
+    			     GPIOB->PTOR =1<<7;
+    			     contador_ledgreen=0;
+    			    } //led verde
+    			    if(nuevo_byte == 'B' && contador_ledblue==0){
+    			    	GPIOB->PTOR =1<<10;
+    			    	contador_ledblue=contador_ledblue+1;
+    			    			    			}
+    			    	if(nuevo_byte == 'b' && contador_ledblue!=0){
+    			          GPIOB->PTOR =1<<10;
+    			          contador_ledblue=0;
+    			    		}// led azul
+
     		}else{
     			printf("error\r\n");
     		}
